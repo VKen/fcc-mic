262b1370753d1abf5343b7fc94407b033b5d0d25
VKen <vken85@gmail.com>
2019-12-08 15:22:15 +0800

    added catch error to return error response

--- routes/api.js
+++ routes/api.js
@@ -12,19 +12,25 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
+
   var convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
     .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
+
+      try {
+        var input = req.query.input;
+        var initNum = convertHandler.getNum(input);
+        var initUnit = convertHandler.getUnit(input);
+        var returnNum = convertHandler.convert(initNum, initUnit);
+        var returnUnit = convertHandler.getReturnUnit(initUnit);
+        var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+        res.send(JSON.stringify(toString));
+      } catch (err) {
+        res.send(err.message);
+      }
+
       //res.json
     });
-    
+
 };
