8e7b0189545972d74e491edcb6311a81c93b2335
VKen <vken85@gmail.com>
2019-12-08 15:21:54 +0800

    fleshed out the conversion functionality

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -26,6 +26,28 @@ function ConvertHandler() {
    *
    *
    **/
+  const unit_conversion_map = {
+      'gal': 'l',
+      'l': 'gal',
+      'mi': 'km',
+      'km': 'mi',
+      'lbs': 'kg',
+      'kg': 'lbs'
+  },
+
+  valid_units = Object.keys(unit_conversion_map),  // lowercase
+
+  unit_language_map = {  // using American Spelling, plural
+      'gal': 'gallons',
+      'l': 'liters',
+      'mi': 'miles',
+      'km': 'kilometers',
+      'lbs': 'pounds',
+      'kg': 'kilograms'
+  },
+
+  invalid_error = ["number", "unit", "number and unit"];
+
   function splitInputPart(input, type) {
     if (!["number", "unit"].includes(type)) {
         throw new Error("invalid or missing `type` input!");
@@ -47,10 +69,6 @@ function ConvertHandler() {
     // workaround without lookbehind
     // take in optional fractions and decimals with a measurement unit
 
-    valid_units = ['gal','l','mi','km','lbs','kg'],  // lowercase
-
-    invalid_error = ["number", "unit", "number and unit"],
-
     checkUnitPattern = "[a-z]+$", // unit at end of string
 
     checkNumberPattern = "^"  // number part at start of string
@@ -67,10 +85,10 @@ function ConvertHandler() {
 
     checkFormatPattern = checkNumberPattern + checkUnitPattern;
 
-    let formatMatchPosition = cleaned.search(new RegExp(checkFormatPattern, 'i'),
+    let formatMatchPosition = cleaned.search(new RegExp(checkFormatPattern, 'i')),
         unitPosition = cleaned.search(new RegExp(checkUnitPattern, 'i'));
 
-    if (formatMatchPosition) === -1){
+    if (formatMatchPosition === -1){
         // no match at all
         if (unitPosition > 0) throw new Error(invalid_error[0]); // can detect unit, then number is the problem
         // can't detect unit, so both number and unit is the problem
@@ -88,46 +106,63 @@ function ConvertHandler() {
 
     // last layer of validation
     //if (!valid_units.include(result.unit.toLowerCase())) return invalid_error[1];
-    if (!valid_units.include(result.unit.toLowerCase())) throw new Error(invalid_error[1]);
+    if (!valid_units.includes(result.unit.toLowerCase())) throw new Error(invalid_error[1]);
 
     if (type == 'number') return result.number;
     if (type == 'unit') return result.unit;
   }
 
   this.getNum = function(input) {
-    var result;
     return splitInputPart(input, 'number');
   };
 
   this.getUnit = function(input) {
-    var result;
     return splitInputPart(input, 'unit');
   };
 
   this.getReturnUnit = function(initUnit) {
-    var result;
-
-    return result;
+    return unit_conversion_map[initUnit.toLowerCase()];
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
-
-    return result;
+    return unit_language_map[unit];
   };
 
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
 
-    return result;
+    switch(initUnit){
+        case 'gal':
+            return initNum / galToL;
+            break;
+        case 'l':
+            return initNum * galToL;
+            break;
+        case 'lbs':
+            return initNum / lbsToKg;
+            break;
+        case 'kg':
+            return initNum * lbsToKg;
+            break;
+        case 'mi':
+            return initNum / miToKm;
+            break;
+        case 'km':
+            return initNum * miToKm;
+            break;
+    }
   };
 
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-
+    var result = {
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`,
+    }
     return result;
   };
 
